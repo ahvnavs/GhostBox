69e7de06772909e3bb3a5943065653b44b6eb463
Abhinav Sharma <ahvnav01@gmail.com>
2026-06-08 17:52:14 +0000

    network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,1 +1,46 @@
+resource "aws_vpc" "vpc_main" {
+    cidr_block = ""
+    enable_dns_hostnames = true
+    enable_dns_support = true
+    enable_network_address_usage_metrics = true
+    region = var.def_region
+}
 
+resource "aws_subnet" "pub_sub_01" {
+    vpc_id = aws_vpc.vpc_main.id
+    availability_zone = "ap-south-1a"
+    cidr_block = ""
+    map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "pub_sub_02" {
+    vpc_id = aws_vpc.vpc_main.id
+    availability_zone = "ap-south-1b"
+    cidr_block = ""
+    map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "pri_sub_01" {
+    vpc_id = aws_vpc.vpc_main.id
+    availability_zone = "ap-south-1a"
+    cidr_block = ""
+}
+
+resource "aws_subnet" "pri_sub_02" {
+    vpc_id = aws_vpc.vpc_main.id
+    availability_zone = "ap-south-1b"
+    cidr_block = ""
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.vpc_main.id
+}
+resource "aws_route_table" "route" {
+    vpc_id = aws_vpc.vpc_main.id
+}
+
+resource "aws_route_table_association" "route_ass" {
+    route_table_id = aws_route_table.route.id
+    gateway_id = aws_internet_gateway.igw.id
+    subnet_id = ""
+}
